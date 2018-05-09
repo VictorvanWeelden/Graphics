fe44b0f298baf85f6d31bb94453e54c5c90bff86
Elise <elise.weusthuis@me.com>
2018-05-09 11:36:09 +0200

    Poging tot plane intersect
    niet 100% zeker of dit legit wiskunde is

--- Plane.cs
+++ Plane.cs
@@ -8,10 +8,27 @@ namespace template
 {
     class Plane : Primitive
     {
-        float lb;
-        float rb;
-        float lo;
+        V3 lb;
+        V3 rb;
+        V3 lo;
+        V3 n;
+        V3 p;
+        float d;
 
-       // public Plane(V3 linksboven, V3)
+        public Plane(V3 linksboven, V3 rechtsboven, V3 linksonder, float D)
+        {
+            lb = linksboven;
+            rb = rechtsboven;
+            lo = linksonder;
+            d = D;
+        }
+
+        void PlaneIntersect(Ray ray)
+        {
+            n = new V3(lb.X, rb.Y, lo.Z);
+            float t = (V3.Dot(ray.O, n) + d) / (V3.Dot(ray.D, n));
+            V3 p = ray.O + (t * ray.D);
+        }
+
     }
 }
