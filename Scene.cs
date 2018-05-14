2eb8a33edce516058c482fe15754d97959ce8a6a
Elise <elise.weusthuis@me.com>
2018-05-14 19:27:21 +0200

    scene update
    heb de scene geprobeerd te maken zoals in het voorbeeld

--- Scene.cs
+++ Scene.cs
@@ -13,13 +13,18 @@ namespace template
 
         V3 lightPositie = new V3(0, 0, 0);
         V3 lightKleur = new V3(1, 1, 0);
-        V3 sphereVector = new V3(1, 1, 1);
+        V3 sphere1Positie = new V3(3, 0, 3);
+        V3 sphere2Positie = new V3(5, 0, 3);
+        V3 sphere3Positie = new V3(8, 0, 3);
+        int radius = 1;
 
         public Scene()
         {
             Light light = new Light(lightPositie, lightKleur);
-            Sphere sphere = new Sphere(3, sphereVector);
-            //Plane plane = new Plane(); //hier moeten wel variabelen in
+            Sphere sphere1 = new Sphere(radius, sphere1Positie);
+            Sphere sphere2 = new Sphere(radius, sphere2Positie);
+            Sphere sphere3 = new Sphere(radius, sphere3Positie);
+            Plane plane = new Plane(new V3(0,-1,0), new V3(10,-1,0), new V3(0,-1,10), 1); //Ik snap niet echt wat D hier moet zijn, kloppen de hoeken zo?
    
         }
 
