378ea52d92f92362464af0d8eb5247c07f90c293
Elise <elise.weusthuis@me.com>
2018-05-29 12:26:54 +0200

    camera beweegbaar

--- Camera.cs
+++ Camera.cs
@@ -15,16 +15,21 @@ namespace template
         public Vector3 linksonder;
         public float horrichting = 0f;
         public float verrichting = 0f;
-        public Vector3 richting = Vector3.Zero;
+        public Vector3 richting = new Vector3(0,0,1);
         public Vector3 pos;
+        public Vector3 up = new Vector3(0, 1, 0);
 
         public Camera(Vector3 position)
         {
             pos = position;
-            linksboven = new Vector3(position.X - schermxy, position.Y + schermxy, position.Z + schermz);
-            rechtsboven = new Vector3(position.X + schermxy, position.Y + schermxy, position.Z + schermz);
-            linksonder = new Vector3(position.X -schermxy, position.Y -schermxy, position.Z + schermz);
-            TurnCamera(50, 50);
+            var u = Vector3.Cross(up, richting);
+            var v = Vector3.Cross(u, richting);
+            var w = new Vector3 (pos + (richting * schermz));
+
+            linksboven = new Vector3(w - u - v);
+            rechtsboven = new Vector3(w + u - v);
+            linksonder = new Vector3(w -u + v);
+            TurnCamera(0, 0);
         }
 
         public void MoveCamera(Vector3 xyz)
@@ -37,9 +42,16 @@ namespace template
 
         public void TurnCamera(float horgraden, float vertgraden)
         {
-            horrichting += horgraden;
-            verrichting += vertgraden;
-            richting = pos + (float)Math.Sqrt((schermz * schermz) + (double)(schermxy * schermxy)) * new Vector3((float)(Math.Sin(verrichting) * Math.Cos(horrichting)), (float)(Math.Sin(verrichting) * Math.Sin(horrichting)), (float)(Math.Cos(verrichting)));
+            horrichting += (float)(horgraden * Math.PI / 180);
+            verrichting += (float)(vertgraden * Math.PI / 180);
+            richting = Vector3.Normalize( pos + schermz  * new Vector3((float)(Math.Sin(horrichting) * Math.Cos(verrichting)), (float)(Math.Sin(horrichting) * Math.Sin(verrichting)), (float)(Math.Cos(horrichting))));
+            var u = Vector3.Cross(up, richting);
+            var v = Vector3.Cross(u, richting);
+            var w = new Vector3(pos + (richting * schermz));
+
+            linksboven = new Vector3(w - u - v);
+            rechtsboven = new Vector3(w + u - v);
+            linksonder = new Vector3(w - u + v);
 
         }
         
